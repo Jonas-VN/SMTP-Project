827beb2b30fd004b417198c118228790e127dd23
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2023-05-01 16:47:44 +0200

    remove file

--- test.py
+++
@@ -1,9 +0,0 @@
-import dns.resolver
-
-mx_records = dns.resolver.Resolver().resolve("gmail.com", "MX")
-mx_records = sorted(mx_records, key=lambda x: x.preference)
-
-for mx in mx_records:
-    preference = mx.preference
-    smtp_server = mx.exchange.to_text().rstrip('.')
-    print(f"Preference: {preference}\t SMTP Server: {smtp_server}")
